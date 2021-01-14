33b08084eea4de90aef59393b23e4160b289cd26
yyhu <easyyhu@gmail.com>
2021-01-14 21:01:59 +0800

    20210114

--- 11 Distributed/SpringCloudDemo/cloud-provider-payment8001/src/main/java/com/huyy/springcloud/PaymentApp8001.java
+++ 11 Distributed/SpringCloudDemo/cloud-provider-payment8001/src/main/java/com/huyy/springcloud/PaymentApp8001.java
@@ -3,6 +3,7 @@ package com.huyy.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author yyhu
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  **/
 @SpringBootApplication
 @MapperScan("com.huyy.springcloud.mapper")
+@EnableEurekaClient
 public class PaymentApp8001 {
 
     public static void main(String[] args) {
