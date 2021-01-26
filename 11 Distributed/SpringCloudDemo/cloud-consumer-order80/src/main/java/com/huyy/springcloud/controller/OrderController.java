f30efd442baf4e86161555db36dc835be1e2b95f
yyhu <easyyhu@gmail.com>
2021-01-26 21:12:33 +0800

    20210126

--- 11 Distributed/SpringCloudDemo/cloud-consumer-order80/src/main/java/com/huyy/springcloud/controller/OrderController.java
+++ 11 Distributed/SpringCloudDemo/cloud-consumer-order80/src/main/java/com/huyy/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.huyy.springcloud.controller;
 import com.huyy.springcloud.bean.CommonResult;
 import com.huyy.springcloud.bean.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,10 +32,43 @@ public class OrderController{
         return commonResult;
     }
 
+    //getForObject方式
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         CommonResult commonResult = restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
         return commonResult;
     }
 
+    //getForEntity方式
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id)
+    {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+
+        if(entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else{
+            return new CommonResult<>(444,"操作失败");
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
